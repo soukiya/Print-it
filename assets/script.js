ee305038b31867b63d9529f949fee21288c265e8
soukaina bizi <soukaina.bizi@gmail.com>
2023-08-07 10:32:06 +0200

    add bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
 const arrowRight =document.querySelector(".arrow_right")
 const arrowLeft =document.querySelector(".arrow_left")
+const bannerImage =document.querySelector(".banner-img")
+const dotsElement =document.querySelectorAll(".dot")
+
 
 
 const slides = [
@@ -21,9 +24,35 @@ const slides = [
 	}
 ]
 
+let numSlide =0; 
+bannerImage.setAttribute("src","./assets/images/slideshow/"+slides[numSlide].image)
+console.log(slides.length)
 arrowRight.addEventListener("click",function(){
-	console.log("Bonjour")
+	numSlide ++;
+	if(numSlide >= slides.length){
+		numSlide=0 ;
+		dotsElement[slides.length-1].classList.remove("dot_selected")
+		dotsElement[numSlide].classList.add("dot_selected")
+		
+
+	}else{
+		dotsElement[numSlide -1].classList.remove("dot_selected")
+		dotsElement[numSlide].classList.add("dot_selected")
+	}
+	
+	
+	bannerImage.setAttribute("src","./assets/images/slideshow/"+slides[numSlide].image)
+	
 })
+
+
+
 arrowLeft.addEventListener("click",function(){
-	console.log("bonsoir")
+		numSlide --;
+	if(numSlide <0 ){
+		numSlide=4 ;
+	
+	}
+	bannerImage.setAttribute("src","./assets/images/slideshow/"+slides[numSlide].image)
+	
 })
